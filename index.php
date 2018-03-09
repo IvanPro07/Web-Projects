e50e92f47c68a51ebb1eab7b2bb0ea091982ddac
IvanPro07 <36634397+IvanPro07@users.noreply.github.com>
2018-03-09 14:37:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+  	<form action="index.php">
+    	<input type="text" name="txt"/>
+    </form>
+    <?php
+		echo "Всё получилось";
+    ?>
+  </body>
+</html>
